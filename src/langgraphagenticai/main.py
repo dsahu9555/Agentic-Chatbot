1fa0fb21d75b2b7dc69a34137dfc1a8fe445b469
Deepak Sahu <dsahu9555@gmail.com>
2026-06-12 23:42:58 +0530

    Agentic chatbot updates

--- src/langgraphagenticai/main.py
+++ src/langgraphagenticai/main.py
@@ -1,6 +1,8 @@
 import streamlit as st
-
+from src.langgraphagenticai.LLMS.groqllm import GroqLLM
 from src.langgraphagenticai.ui.streamlit.loadui import LoadStreamlitUI
+from src.langgraphagenticai.graph.graph_builder import GraphBuilder
+from src.langgraphagenticai.ui.streamlit.display_result import DisplayResultStreamlit
 
 def load_langgraph_agenticai_app():
     """ 
@@ -17,7 +19,56 @@ def load_langgraph_agenticai_app():
     if not user_input:
         st.error("Error: Failed to load user input from the UI.")
         return 
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
     
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
     user_message = st.chat_input("Enter your message:")
 
-    # if user_message:
+    if user_message:
+
+        st.session_state.messages.append(
+
+            {
+
+                "role": "user",
+
+                "content": user_message
+
+            }
+
+        )
+
+    if user_message:
+        try:
+            ## Configure The LLM
+            obj_llm_config = GroqLLM(user_controls_input=user_input)
+            model = obj_llm_config.get_llm_model()
+
+            if not model:
+                st.error("Error: LLM model could not be initialized")
+                return
+            
+            #Initialize and set up the graph based on use case
+            usecase = user_input.get("selected_usecase")
+
+            if not usecase:
+                st.error("Error: No use case selected.")
+                return
+
+            ## Graph Builder
+
+            graph_builder = GraphBuilder(model)
+            try:
+                graph = graph_builder.setup_graph(usecase)
+                DisplayResultStreamlit(usecase,graph, user_message).display_result_on_ui()
+            except Exception as e:
+                st.error(f"Error: Graph set up failed. {e}")
+                return
+        
+        except Exception as e:
+            raise ValueError(f"Error occured with Exception: {e}")
+            return
--- src/langgraphagenticai/ui/streamlit/loadui.py
+++ src/langgraphagenticai/ui/streamlit/loadui.py
@@ -26,7 +26,7 @@ class LoadStreamlitUI:
                 model_options = self.config.get_groq_model_options()
                 self.user_controls["selected_groq_model"] = st.selectbox("Select Model", model_options)
                 self.user_controls["GROQ_API_KEY"]= st.session_state["GROQ_API_KEY"] =st.text_input("API key",type="password")
-                # validate api key
+            
                 if not self.user_controls["GROQ_API_KEY"]:
                     st.warning(" Please enter your GRQO API KEY to proceed.")
                 
